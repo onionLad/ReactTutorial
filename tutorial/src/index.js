2015e1d8b1dace1117632f5e71c55acbfe867dee
Bill <bill.xia.pa@gmail.com>
2022-12-20 21:35:02 -0500

    Completed Tutorial
    We've (kinda) learned the ropes of React. Let's see if we can transfer our knowledge to something else!

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -37,64 +37,32 @@ function Square(props) {
 /* Defines the Board class, which is a component of the Game class. */
 class Board extends React.Component {
 
-    /*
-     * constructor
-     * Initializes Board class members.
-     */
-    constructor(props) {
-        super(props);
-        this.state = {
+    // /*
+    //  * constructor
+    //  * Initializes Board class members.
+    //  */
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
 
-            /* Array that keeps track of the state of each square. */
-            squares: Array(9).fill(null),
+    //         /* Array that keeps track of the state of each square. */
+    //         squares: Array(9).fill(null),
 
-            /* Keeps track of which player's turn it currently is. */
-            xIsNext: true
+    //         /* Keeps track of which player's turn it currently is. */
+    //         xIsNext: true
 
-        };
-    }
-
-    /*
-     * handleClick
-     * Updates the Board state.
-     */
-    handleClick(i) {
-        
-        /* 
-         * Forgive the terrible variable names. I'm just following the
-         * tutorial.
-         * 
-         * I'm pretty sure this function gets a local version of the squares
-         * member, updates it, then transfers the local change to the actual
-         * class member.
-         */
-        const squares = this.state.squares.slice();
-
-        /* 
-         * Filthy chunk of code. Uses the "not false" bool trick. Also shows
-         * off JavaScript's disregard for types with the return nothing line.
-         */
-        if (calculateWinner(this.state.squares) || squares[i]) {
-            return;
-        }
-
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext
-        });
-
-    }
+    //     };
+    // }
 
     renderSquare(i) {
         return (
             <Square 
 
                 /* Render the square using data from the squares member. */
-                value={this.state.squares[i]}
+                value={this.props.squares[i]}
 
                 /* This function updates Board when Square gets clicked. */
-                onClick={ () => this.handleClick(i) }
+                onClick={ () => this.props.onClick(i) }
 
             />
         );
@@ -102,24 +70,23 @@ class Board extends React.Component {
 
     render() {
         
-        /* Display current game status. */
-        const winner = calculateWinner(this.state.squares);
-        let status;
+        // /* Display current game status. */
+        // const winner = calculateWinner(this.state.squares);
+        // let status;
 
-        /* 
-         * This trick works because anything that isn't 0 (null) evaluates to
-         * true. That is, a non-boolean value gets interpretted as a bool.
-         */
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
+        // /* 
+        //  * This trick works because anything that isn't 0 (null) evaluates to
+        //  * true. That is, a non-boolean value gets interpretted as a bool.
+        //  */
+        // if (winner) {
+        //     status = 'Winner: ' + winner;
+        // } else {
+        //     status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        // }
 
         /* Render the Board. */
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -142,18 +109,130 @@ class Board extends React.Component {
 
 /* Defines the Game class. */
 class Game extends React.Component {
+    
+    /* Moving more operations into the Game class. */
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }],
+            stepNumber: 0,
+            xIsNext: true
+        };
+    }
+
+    /*
+     * handleClick
+     * Updates the Board state.
+     */
+    handleClick(i) {
+        
+        /*
+         * This line ensures that history erases the future if changes to the
+         * past are made.
+         */
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    
+        /* Current time state. */
+        const current = history[history.length - 1];
+        
+        /* 
+         * Forgive the terrible variable names. I'm just following the
+         * tutorial.
+         * 
+         * I'm pretty sure this function gets a local version of the squares
+         * member, updates it, then transfers the local change to the actual
+         * class member.
+         */
+        const squares = current.squares.slice();
+
+        /* 
+         * Filthy chunk of code. Uses the "not false" bool trick. Also shows
+         * off JavaScript's disregard for types with the return nothing line.
+         */
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        /* Setting current square to display X or O. */
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+        this.setState({
+            
+            /* 
+             * This block appends the current boardstate to the end of the
+             * game's history. concat() is used instead of push() because it
+             * doesn't mutate history.
+             */
+            history: history.concat([{
+                squares: squares
+            }]),
+
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext
+        });
+
+    }
+
+    /* This function reset the Boardstate to a previous version. */
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
+        });
+    }
+    
     render() {
+        
+        const history = this.state.history;             // list of boardstates
+        const current = history[this.state.stepNumber]; // current state
+        
+        /* Optaining victory status. */
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        /*
+         * Displaying Game history using a map function. Note that step is
+         * never used; it's just there to satisfy the parameters of map().
+         */
+        const moves = history.map((step, move) => {
+
+            /*
+             * Once again using a bool trick to determine if a given tag
+             * refers to a move or the start o the game (move 0).
+             */
+            const tag = move ? 'Go to move #' + move : 'Go to start';
+
+            /* Maps a button to the desired Boardstate. */
+            return (
+                <li key={move}>
+                    <button onClick={ () => this.jumpTo(move) }>{tag}</button>
+                </li>
+            )
+
+        });
+        
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={ (i) => this.handleClick(i) }
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
+
     }
 }
 
